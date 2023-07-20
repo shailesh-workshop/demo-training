cd30db399ac02147e8372ffe5ea0fc44fc82eb4a
Shailesh Dinde <shdinde@gmail.com>
2023-07-20 10:16:17 +0530

    Added code for pwersonal website

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <h1>This is First line by dev1</h1>
+        <h1>This is Second line by dev1</h1>
+        <h1>This line by dev1</h1>
+        <h1>This is First line by dev2</h1>
+        <h1>This is Second line changed by dev 2</h1>
+        <h1>This is extra line by dev2</h1>
+        <h1>This is fourth line by dev2</h1>
+    </body>
+</html>
